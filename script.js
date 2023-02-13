c51097b36a4ae9c7d34bfb29bd1ed3d4431563a5
ibraheem mariam <ibraheemmaryam26@gmail.com>
2023-02-13 22:44:44 +0100

    commit

--- script.js
+++ script.js
@@ -19,14 +19,25 @@ const playPause = () => {
     if(video.paused){
         video.play()
         btn.className = 'far fa-pause-circle'
+        video.style.opacity = '0.7'
     }else{
         video.pause()
         btn.className = 'far fa-play-circle'
+        video.style.opacity = '0.3'
     }
     
 }
 
 btn.addEventListener('click', () => {
     playPause()
+})
+
+video.addEventListener('timeupdate',() => {
+    const barWidth = video.currentTime/video.duration
+    bar.style.width = `${barWidth * 100}%`
+    if(video.ended){
+        btn.className = 'far fa-play-circle'
+        video.style.opacity = '0.3'
+    }
 })
 // end of section 2 video
